d01eda0177750a1345ed2320310237cbef39f2c0
Shrinidhi MK <mkshrinidhi88@gmail.com>
2022-03-07 23:44:38 +0530

    updated mod 2

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,21 +1,20 @@
 import React, { Component } from 'react'
 
-class QuizQuestion extends Component{
-        render() {
-            return(
-                <main>
+class QuizQuestion extends Component {
+  render() {
+    return (
+      <main>
         <section>
-          <p>{this.props.quiz_question.instruction_text} </p>
+          <p>{this.props.quiz_question.instruction_text}</p>
         </section>
         <section className="buttons">
           <ul>
-            <li> {this.props.quiz_question.answer_options[0]} </li>
+            <li>{this.props.quiz_question.answer_options[0]}</li>
           </ul>
         </section>
-      </main>
-            )
-        }
-    }
+      </main>      
+    )
+  }
+}
 
-
-export default QuizQuestion
+export default QuizQuestion 
